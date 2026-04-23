5661e792d6f586bf4d7b9c19be7919fff72ae585
me <end3624@gmail.com>
2026-04-23 13:25:56 +0800

    2021-09-30 12:00:00 +0000

---
+++ ex9.py
@@ -0,0 +1,2 @@
+exam_st_date = input("Enter the examination start date (dd/mm/yyyy): ").split("/")  
+print("The examination will start from : {} / {} / {}".format(exam_st_date[0], exam_st_date[1], exam_st_date[2]))
